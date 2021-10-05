3b1c3daee0da76e771cd16693d0412cd7940be24
DanielAlgaba <2001dav@gmail.com>
2021-10-05 20:41:07 +0200

    Added new function .born

--- johnson/johnson.py
+++ johnson/johnson.py
@@ -3,6 +3,7 @@ import discord
 import random
 import glob
 from johnson_quotes import quotes
+from datetime import date
 
 client = discord.Client()
 
@@ -17,14 +18,21 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith(".image"):
-        images = glob.glob(".\johnson\ArtOfHaloInfiniteHD\*.jpg")
+        images = glob.glob("/workspaces/discord_bots/johnson/ArtOfHaloInfiniteHD/*.jpg")
         random_image = random.choice(images)
         await message.channel.send(
-            "There you go, Marine!", file=discord.File(random_image),
+            "There you go, Marine!",
+            file=discord.File(random_image),
         )
 
     if message.content.startswith(".sir"):
         await message.channel.send(random.choice(quotes))
+    if message.content.startswith(".born"):
+        d = date(2552, 12, 11)
+        delta = d - date.today()
+        await message.channel.send(
+            f"There are {delta.days} days left for me to be born!"
+        )
 
 
 client.run(os.environ["DISCORD_TOKEN"])
