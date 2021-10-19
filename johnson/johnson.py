12fcb087f61557d6f6fbd49c5d637721b8d51134
DanielAlgaba <2001dav@gmail.com>
2021-10-19 17:59:31 +0200

    Fixed the Gamertag sended on discord and added some messages effects

--- johnson/johnson.py
+++ johnson/johnson.py
@@ -30,18 +30,18 @@ async def on_message(message):
     if message.content.startswith(".help"):
         await message.channel.send(
             (
-                "These are the avialable commands:\n"
-                "- .image:\t\t   Johnson sends"
+                "**These are the avialable commands**:\n"
+                "- **.image**:\t\t    Johnson sends"
                 " a ramdom Halo Infinite art image.\n"
-                "- .sir:\t\t\t\t  Johnson sends a random quote.\n"
-                "- .born:\t\t\t  days left for Johnson to be born.\n"
-                "- .infinite:\t\t  days left for Halo Infinite release.\n"
-                "- .camaign:\t\tfollowed by a Gamertag"
+                "- **.sir**:\t\t\t\t   Johnson sends a random quote.\n"
+                "- **.born**:\t\t\t   days left for Johnson to be born.\n"
+                "- **.infinite**:\t\t   days left for Halo Infinite release.\n"
+                "- **.camaign**:\t\t followed by a Gamertag"
                 " to see its campaign stats.\n"
-                "- .multiplayer:\tfollowed by a Gamertag "
+                "- **.multiplayer**:\tfollowed by a Gamertag "
                 "to see its multiplayer stats\n"
-                "- .avatar:\t\t\tfollowed by a Gamertag to see "
-                "its emblem, avatar, clan tag and service tag\n"
+                "- **.avatar**:\t\t\t followed by a Gamertag to see "
+                "its emblem, avatar, clan and service tag\n"
             )
         )
 
@@ -52,13 +52,13 @@ async def on_message(message):
         )
         random_image = random.choice(images)
         await message.channel.send(
-            "There you go, Marine!",
+            "**There you go, Marine!**",
             file=discord.File(random_image),
         )
 
     # The bot sends a message with a random quote
     if message.content.startswith(".sir"):
-        await message.channel.send(random.choice(quotes))
+        await message.channel.send(f"**{random.choice(quotes)}**")
 
     # The bot sends a message with the days left for
     # the character Avery J. Johnson to be born in Halo lore
@@ -66,7 +66,7 @@ async def on_message(message):
         d = date(2552, 12, 11)
         delta = d - date.today()
         await message.channel.send(
-            f"There are {delta.days} days left for me to be born!"
+            f"There are **{delta.days}** days left for me to be born!"
         )
 
     # The bot send a message with the days left
@@ -77,13 +77,17 @@ async def on_message(message):
         if today < d:
             delta = d - today
             await message.channel.send(
-                f"There are {delta.days} days left for Halo Infinite launch!"
+                (
+                    f"There are **{delta.days}** days"
+                    f" left for"
+                    f" Halo Infinite launch!"
+                )
             )
         else:
             await message.channel.send(
                 (
                     "Halo Infinite is out! What are you waiting for Marine?!"
-                    " Go play right now!"
+                    " **Go play right now!**"
                 )
             )
 
@@ -104,6 +108,7 @@ async def on_message(message):
             }
             response = requests.request("GET", url, headers=headers)
             stats = json.loads(response.text)
+            player = stats["additional"]["gamertag"]
             missions = stats["data"]["campaign"]["missions"]["completed"]
             missions_kills = stats["data"]["campaign"]["missions"]["kills"]
             missions_deaths = stats["data"]["campaign"]["missions"]["deaths"]
@@ -113,39 +118,24 @@ async def on_message(message):
 
             await message.channel.send(
                 (
-                    f"{player} has completed {missions} missions, has killed "
-                    f"{missions_kills} enemies and has died "
-                    f"{missions_deaths} times in campaign."
+                    f"**{player}** has completed"
+                    f" **{missions}** missions, has killed "
+                    f"**{missions_kills}** enemies and has died "
+                    f"**{missions_deaths}** times in campaign."
                 )
             )
             await message.channel.send(
                 (
-                    f"{player} has completed {playlists} playlists, "
-                    f"has killed {playlists_kills} enemies and "
-                    f"has died {playlists_deaths} times in playlists."
+                    f"**{player}** has completed **{playlists}** playlists, "
+                    f"has killed **{playlists_kills}** enemies and "
+                    f"has died **{playlists_deaths}** times in playlists."
                 )
             )
         except KeyError:
             await message.channel.send(
                 (
-                    "An error has occurred! You have not written any Gamertag"
-                    " or the one you have entered does not exit!"
-                )
-            )
-
-    if message.content.startswith(".infinite"):
-        d = date(2021, 12, 8)
-        today = date.today()
-        if today < d:
-            delta = d - today
-            await message.channel.send(
-                f"There are {delta.days} days left for Halo Infinite launch!"
-            )
-        else:
-            await message.channel.send(
-                (
-                    "Halo Infinite is out! What are you waiting for Marine?!"
-                    " Go play right now!"
+                    "`An error has occurred! You have not written any Gamertag"
+                    " or the one you have entered does not exit!`"
                 )
             )
 
@@ -166,6 +156,7 @@ async def on_message(message):
             }
             response = requests.request("GET", url, headers=headers)
             stats = json.loads(response.text)
+            player = stats["additional"]["gamertag"]
             matches = stats["data"]["multiplayer"]["total_matches"]
             kd_ratio = stats["data"]["multiplayer"]["kdr"]
             kills = stats["data"]["multiplayer"]["summary"]["kills"]
@@ -182,28 +173,29 @@ async def on_message(message):
 
             await message.channel.send(
                 (
-                    f"{player} has a k/d of {str(round(kd_ratio, 2))} "
-                    f"with {kills} kills, {deaths} deaths and "
-                    f"{assists} assists."
+                    f"**{player}** has a k/d of"
+                    f" **{str(round(kd_ratio, 2))}** "
+                    f"with **{kills}** kills, **{deaths}** deaths and "
+                    f"**{assists}** assists."
                 )
             )
             await message.channel.send(
                 (
-                    f"{player} has a win/lose ratio "
-                    f"of {str(round(wl_ratio, 2))} "
-                    f"in {matches} matches. "
-                    f"{player} has won {wins} matches and"
-                    f" has lost {loses} matches."
+                    f"**{player}** has a win/lose ratio "
+                    f"of **{str(round(wl_ratio, 2))}** "
+                    f"in **{matches}** matches. "
+                    f"**{player}** has won **{wins}** matches and"
+                    f" has lost **{loses}** matches."
                 )
             )
             await message.channel.send(
-                f"{player} max rank was {rank_level} in {rank_pl}."
+                (f"**{player}** max rank " f"was **{rank_level}** in **{rank_pl}**.")
             )
         except KeyError:
             await message.channel.send(
                 (
-                    "An error has occurred! You have not written any Gamertag"
-                    " or the one you have entered does not exit!"
+                    "`An error has occurred! You have not written any Gamertag"
+                    " or the one you have entered does not exit!`"
                 )
             )
 
@@ -224,19 +216,20 @@ async def on_message(message):
             }
             response = requests.request("GET", url, headers=headers)
             stats = json.loads(response.text)
+            player = stats["additional"]["gamertag"]
             clan = stats["data"]["clan_tag"]
             emblem = stats["data"]["emblem_url"]
             avatar = stats["data"]["avatar_url"]
             tag = stats["data"]["service_tag"]
 
             await message.channel.send(emblem)
-            await message.channel.send((f"[{clan}] {player} {tag} "))
+            await message.channel.send((f"[**{clan}**] " f"{player}" f" *" f"*{tag}**"))
             await message.channel.send(avatar)
         except KeyError:
             await message.channel.send(
                 (
-                    "An error has occurred! You have not written any Gamertag"
-                    " or the one you have entered does not exit!"
+                    "`An error has occurred! You have not written any Gamertag"
+                    " or the one you have entered does not exit!`"
                 )
             )
 
